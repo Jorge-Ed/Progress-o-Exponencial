4c323ee72f77bfbef79049e4d879bcc72b6f5267
Jorge Edson <60950040+Jorge-Ed@users.noreply.github.com>
2020-03-03 22:12:34 -0300

    Add files via upload

---
+++ 120.py
@@ -0,0 +1,9 @@
+#Autor:1DSI3
+	#@Jorge Edson
+	#Instituição: ETEC
+x=0 #Declaração da Variável Contador.
+Valor=0 #Declaração da Variável Valor.
+While (x <= 21):
+	valor=valor
+	x = x + 1 #Laço de Repetição.
+	print(Valor) #Exibir a Variável Valor.
